df632fc10463b0c96409456bd25a70ef0eacf4d8
anonimi862 <121517315+anonimi862@users.noreply.github.com>
2025-04-21 18:47:40 +0300

    Add files via upload

---
+++ stage 1/sqlTables/dropTables.sql
@@ -0,0 +1,6 @@
+DROP TABLE Orders;
+DROP TABLE Supplier;
+DROP TABLE Warehouse;
+DROP TABLE Employee;
+DROP TABLE Part;
+DROP TABLE Train;
---
+++ stage 1/sqlTables/selectAll.sql
@@ -0,0 +1,6 @@
+SELECT * FROM Train;
+SELECT * FROM Part;
+SELECT * FROM Employee;
+SELECT * FROM Warehouse;
+SELECT * FROM Supplier;
+SELECT * FROM Orders;
